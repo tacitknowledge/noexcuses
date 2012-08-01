88ae7a755369ea72f91322560f489bd3b406a67d
Ivan Frolov <ifrolov@tacitknowledge.com>
2012-08-01 17:41:22 +0300

    Covering DummyObjectBuilder.

--- src/test/java/com/tacitknowledge/noexcuses/SampleTest.java
+++ src/test/java/com/tacitknowledge/noexcuses/SampleTest.java
@@ -10,12 +10,12 @@ import org.junit.Test;
  * Created by IntelliJ IDEA. User: mshort Date: May 3, 2011 Time: 9:39:57 AM To
  * change this template use File | Settings | File Templates.
  */
+@SuppressWarnings("unchecked")
 public class SampleTest {
 	@Test
 	public void testAllMethods() throws Exception {
 		DummyObject object = new DummyObject("Something", "another", true);
-		MethodTester methodTester = new MethodTester(
-				ExceptionHandler.SILENT_ON_EXCEPTION);
+		MethodTester methodTester = new MethodTester(ExceptionHandler.SILENT_ON_EXCEPTION);
 		methodTester.performTest(object);
 	}
 
@@ -27,21 +27,17 @@ public class SampleTest {
 	}
 
 	@Test
-	public void testGetterAndSetterConvenienceMethodWithExclusions()
-			throws Exception {
+	public void testGetterAndSetterConvenienceMethodWithExclusions() throws Exception {
 		DummyObject object = new DummyObject("Something", "another", true);
 		MethodTester methodTester = new MethodTester();
-		methodTester.performGettersAndSetters(object,
-				new String[] { "getSomething" });
+		methodTester.performGettersAndSetters(object, new String[] { "getSomething" });
 
 	}
 
 	@Test
-	public void testAllMethodsExceptAttemptAndThrowAnException()
-			throws Exception {
+	public void testAllMethodsExceptAttemptAndThrowAnException() throws Exception {
 		DummyObject object = new DummyObject("Something", "another", true);
-		MethodTester methodTester = new MethodTester("", new String[] {
-				"attempt", "throwAnException" }, null,
+		MethodTester methodTester = new MethodTester("", new String[] {"attempt", "throwAnException" }, null,
 				ExceptionHandler.FAIL_ON_EXCEPTION);
 		methodTester.performTest(object);
 	}
@@ -59,8 +55,17 @@ public class SampleTest {
 	}
 
 	@Test
-	public void testName() throws Exception {
+	public void testNoExceptionOnPassingNullConstructor() throws Exception {
 		DummyObjectBuilder dummyObjectBuilder = new DummyObjectBuilder(Collections.EMPTY_MAP);
 		assertNull(dummyObjectBuilder.createInstance(null));
+	}
+
+	@Test
+	public void testCreateObjectWithNoConstructors() throws Exception {
+		DummyObjectBuilder dummyObjectBuilder = new DummyObjectBuilder(Collections.EMPTY_MAP);
+		assertNull(dummyObjectBuilder.createObject(EmptyInterface.class));
+	}
+
+	interface EmptyInterface {
 	}
 }
